f2092a5b046c2eb22e74abc8f9187fa7e6f9665b
Matthew Conover <matthew.conover1@t-mobile.com>
2018-08-06 14:16:21 -0700

    Broke up code and added un-blocking support

--- main.py
+++ main.py
@@ -2,29 +2,32 @@ import json
 import sys
 import yaml
 from subprocess import call, Popen, PIPE, DEVNULL
+from collections import namedtuple
 
 DEFAULT_ENCODING = 'UTF-8'
+ContainerInfo = namedtuple('Host', ['cont_ip', 'cont_ports'])
+DiegoContainerInfo = namedtuple('DiegoCell', ['diego_ip', 'cont_ip', 'cont_ports'])
 
 
-def main(org, space, appname, cfg):
-    enc = DEFAULT_ENCODING
-    cf = cfg['cf']['cmd'] or 'cf'
-    bosh = cfg['bosh']['cmd'] or 'bosh2'
+def cf_target(org, space, cfg):
+    return call([cfg['cf']['cmd'], 'target', '-o', org, '-s', space])
 
-    if call([cf, 'target', '-o', org, '-s', space]):
-        sys.exit("Failed to target {} and {}. Make sure you are logged in and the names are correct!"
-                 .format(org, space))
 
-    with Popen([cf, 'app', appname, '--guid'], stdout=PIPE, stderr=DEVNULL, encoding=enc) as proc:
+def get_guid(appname, cfg):
+    with Popen([cfg['cf']['cmd'], 'app', appname, '--guid'], stdout=PIPE, stderr=DEVNULL, encoding=DEFAULT_ENCODING) as proc:
         guid = proc.stdout.readline().rstrip('\r\n')
         if proc.returncode:
             sys.exit("Failed retrieving the GUID for the specified app. Make sure {} is in this space!".format(appname))
 
-    hosts = {}
+    return guid
 
-    with Popen('{} -e {} -d {} ssh {}'.format(bosh, cfg['bosh']['env'], cfg['bosh']['cf-dep'], cfg['bosh']['cfdot-dc']),
-               shell=True, stdin=PIPE, stdout=PIPE, stderr=DEVNULL, encoding=enc) as proc:
-        stdout, _ = proc.communicate(input='cfdot actual-lrp-groups | grep --color=never {}\nexit\n'.format(guid), timeout=30)
+
+def get_container_hosts(guid, cfg):
+    hosts = {}
+    with Popen('{} -e {} -d {} ssh {}'.format(cfg['bosh']['cmd'], cfg['bosh']['env'], cfg['bosh']['cf-dep'], cfg['bosh']['cfdot-dc']),
+               shell=True, stdin=PIPE, stdout=PIPE, stderr=DEVNULL, encoding=DEFAULT_ENCODING) as proc:
+        stdout, _ = proc.communicate(input='cfdot actual-lrp-groups | grep --color=never {}\nexit\n'.format(guid),
+                                     timeout=30)
         if proc.returncode:
             sys.exit("Failed retrieving LRP data from {}".format(cfg['bosh']['cfdot-dc']))
 
@@ -55,53 +58,98 @@ def main(org, space, appname, cfg):
                 cont_ports.add(cont_port)
                 print('Found application at {}:{} with container port {}'.format(host_ip, host_port, cont_port))
 
-            hosts[host_ip] = (cont_ip, cont_ports)
+            hosts[host_ip] = ContainerInfo(cont_ip, cont_ports)
 
-        if not hosts:
-            sys.exit("No hosts found!")
+    return hosts
+
+
+def get_diego_vm_name(diego_ip, cfg):
+    cmd = "{} -e {} -d {} vms | grep -P '\s{}\s' | grep -Po '^diego-cell/[a-z0-9-]*'" \
+        .format(cfg['bosh']['cmd'], cfg['bosh']['env'], cfg['bosh']['cf-dep'], diego_ip.replace('.', '\.'))
+
+    with Popen(cmd, shell=True, stdout=PIPE, stderr=DEVNULL, encoding=DEFAULT_ENCODING) as proc:
+        if proc.returncode:
+            print("Failed retrieving VM information from BOSH for {}.".format(diego_ip),
+                  file=sys.stderr)
+            return None
+        diego_vm = proc.stdout.readline().rstrip('\r\n')
+
+    return diego_vm
+
+
+def block_app(diego_vm, dci, cfg):
+    with Popen('{} -e {} -d {} ssh {}'.format(cfg['bosh']['cmd'], cfg['bosh']['env'], cfg['bosh']['cf-dep'], diego_vm),
+               shell=True, stdin=PIPE, stdout=DEVNULL, stderr=DEVNULL, encoding=DEFAULT_ENCODING) as proc:
+
+        for cont_port in dci.cont_ports:
+            proc.stdin.write(
+                'sudo iptables -I FORWARD 1 -d {} -p tcp --dport {} -j DROP\n'.format(dci.cont_ip, cont_port))
+        proc.stdin.write('exit\n')
+        proc.stdin.close()
+
+        return proc.returncode
+
+
+def unblock_app(diego_vm, dci, cfg):
+    with Popen('{} -e {} -d {} ssh {}'.format(cfg['bosh']['cmd'], cfg['bosh']['env'], cfg['bosh']['cf-dep'], diego_vm),
+               shell=True, stdin=PIPE, stdout=DEVNULL, stderr=DEVNULL, encoding=DEFAULT_ENCODING) as proc:
+        for cont_port in dci.cont_ports:
+            proc.stdin.write(
+                'sudo iptables -D FORWARD -d {} -p tcp --dport {} -j DROP\n'.format(dci.cont_ip, cont_port))
+        proc.stdin.write('exit\n')
+        proc.stdin.close()
+
+        return proc.returncode
+
+
+def main(org, space, appname, block, cfg):
+    if cf_target(org, space, cfg):
+        sys.exit("Failed to target {} and {}. Make sure you are logged in and the names are correct!"
+                 .format(org, space))
+
+    guid = get_guid(appname, cfg)
+
+    hosts = get_container_hosts(guid, cfg)
+    if not hosts:
+        sys.exit("No hosts found!")
 
     diego_cells = {}
-    for host_ip, (cont_ip, cont_ports) in hosts.items():
-        cmd = "{} -e {} -d {} vms | grep -P '\s{}\s' | grep -Po '^diego-cell/[a-z0-9-]*'" \
-            .format(bosh, cfg['bosh']['env'], cfg['bosh']['cf-dep'], host_ip.replace('.', '\.'))
+    for host_ip, ci in hosts.items():
+        diego_vm = get_diego_vm_name(host_ip, cfg)
 
-        with Popen(cmd, shell=True, stdout=PIPE, stderr=DEVNULL, encoding=enc) as proc:
-            if proc.returncode:
-                print("Failed retrieving VM information from BOSH for {}.".format(host_ip),
-                      file=sys.stderr)
-                continue
-            dc = proc.stdout.readline().rstrip('\r\n')
+        if diego_vm is None:
+            continue
+        assert diego_cells.get(diego_vm) is None
 
-        assert diego_cells.get(dc) is None
-        diego_cells[dc] = (host_ip, cont_ip, cont_ports)
+        diego_cells[diego_vm] = DiegoContainerInfo(host_ip, ci.cont_ip, ci.cont_ports)
 
-    for dc, (host_ip, cont_ip, cont_ports) in diego_cells.items():
-        print("Targeting {} at {} on {}:{}.".format(host_ip, dc, cont_ip, cont_ports))
+    for diego_vm, dci in diego_cells.items():
+        print("Targeting {} at {} on {}:{}.".format(dci.diego_ip, diego_vm, dci.cont_ip, dci.cont_ports))
 
-    for dc, (host_ip, cont_ip, cont_ports) in diego_cells.items():
-        with Popen('{} -e {} -d {} ssh {}'.format(bosh, cfg['bosh']['env'], cfg['bosh']['cf-dep'], dc),
-                   shell=True, stdin=PIPE, stdout=DEVNULL, stderr=DEVNULL, encoding=enc) as proc:
+    for diego_vm, dci in diego_cells.items():
+        if block:
+            if block_app(diego_vm, dci, cfg):
+                sys.exit("Failed to block {}:{} on {}.".format(dci.cont_ip, dci.cont_ports, diego_vm))
+        else:
+            if unblock_app(diego_vm, dci, cfg):
+                print("WARNING: Failed to unblock {}:{} on {}.".format(dci.cont_ip, dci.cont_ports, diego_vm))
 
-            for cont_port in cont_ports:
-                proc.stdin.write('sudo iptables -I FORWARD 1 -d {} -p tcp --dport {} -j DROP\n'.format(cont_ip, cont_port))
-            proc.stdin.write('exit\n')
-            proc.stdin.close()
-
-            if proc.returncode:
-                sys.exit("Failed to block {}:{} on {}.".format(cont_ip, cont_port, dc))
+    print("Done!")
 
 
 if __name__ == '__main__':
     args = sys.argv[1:]
-    if len(args) not in [3, 4]:
-        print("Usage: cf_block_app <org> <space> <app>, [<config_path>]")
+    if len(args) not in [4, 5]:
+        print("Usage: cf_block_app <block:unblock> <org> <space> <app> [<config_path>]")
         exit(1)
 
-    config_path = args[3] if len(args) == 4 else 'config.yml'
+    config_path = args[4] if len(args) == 5 else 'config.yml'
     with open(config_path, 'r') as file:
         config = yaml.load(file)
 
-    main(args[0], args[1], args[2], config)
+    assert args[0] in ['block', 'unblock']
+
+    main(args[1], args[2], args[3], args[0] == 'block', config)
 
 
 # 75479234-b154-42f2-bd04-0c6d1abc83da
