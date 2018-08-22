2c46e5e07c9d47daf1a4dc25d55d5439d1b4879f
mconove1 <matthew.conover1@t-mobile.com>
2018-08-22 10:49:56 -0700

    Fixed issue with cfg var name

--- cfblocker/probes.py
+++ cfblocker/probes.py
@@ -5,53 +5,53 @@ from cfblocker.app import App
 from cfblocker.util import run_ctk as _run
 
 
-def find_hosts(org: str, space: str, appname: str, cfg: Configuration) -> Dict[str, Any]:
+def find_hosts(org: str, space: str, appname: str, configuration: Configuration) -> Dict[str, Any]:
     """
     Find all hosts of this application in Cloud Foundry.
     :param org: String; Cloud Foundry organization containing the application.
     :param space: String; Cloud Foundry space containing the application.
     :param appname: String; Application in Cloud Foundry which is to be targeted.
-    :param cfg: Configuration; Configuration details, see `README.md`.
+    :param configuration: Configuration; Configuration details, see `README.md`.
     :return: A JSON Object representing the application which was targeted.
     """
     def f():
         app = App(org, space, appname)
-        app.find_hosts(cfg)
+        app.find_hosts(configuration)
         return app
 
     return _run(f, "Finding all application hosts...")
 
 
-def find_services(org: str, space: str, appname: str, cfg: Configuration) -> Dict[str, Any]:
+def find_services(org: str, space: str, appname: str, configuration: Configuration) -> Dict[str, Any]:
     """
     Find all services of this application in Cloud Foundry.
     :param org: String; Cloud Foundry organization containing the application.
     :param space: String; Cloud Foundry space containing the application.
     :param appname: String; Application in Cloud Foundry which is to be targeted.
-    :param cfg: Configuration; Configuration details, see `README.md`.
+    :param configuration: Configuration; Configuration details, see `README.md`.
     :return: A JSON Object representing the application which was targeted.
     """
     def f():
         app = App(org, space, appname)
-        app.find_services(cfg)
+        app.find_services(configuration)
         return app
 
     return _run(f, "Finding all application services...")
 
 
-def find_all(org: str, space: str, appname: str, cfg: Configuration) -> Dict[str, Any]:
+def find_all(org: str, space: str, appname: str, configuration: Configuration) -> Dict[str, Any]:
     """
     Find all hosts and all services for this application in Cloud Foundry.
     :param org: String; Cloud Foundry organization containing the application.
     :param space: String; Cloud Foundry space containing the application.
     :param appname: String; Application in Cloud Foundry which is to be targeted.
-    :param cfg: Configuration; Configuration details, see `README.md`.
+    :param configuration: Configuration; Configuration details, see `README.md`.
     :return: A JSON Object representing the application which was targeted.
     """
     def f():
         app = App(org, space, appname)
-        app.find_hosts(cfg)
-        app.find_services(cfg)
+        app.find_hosts(configuration)
+        app.find_services(configuration)
         return app
 
     return _run(f, "Finding all application hosts and services...")
